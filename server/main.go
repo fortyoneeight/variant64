cafaee9ad835742ec18830e8999de0829a8891d0
Izak Fritz <izakfr@umich.edu>
2022-10-09 18:59:00 -0400

    Add http handler and Dockerfile

--- server/main.go
+++ server/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello, world!\n")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8000", nil)
 }
