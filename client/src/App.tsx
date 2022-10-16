b645a05d0201e3c02191a97ea0f8b91174daf70c
henryfbp <HenryFBP@gmail.com>
2022-10-16 19:19:26 -0400

    get state demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Routing from './Routing';
 import { Link } from "react-router-dom";
+import { RecoilRoot } from 'recoil';
 function App() {
   return <>
-
-    <div className='grid-3-vertical-centerbias'>
-
-      <Routing />
-    </div>
+    <RecoilRoot>
+      <div className='grid-3-vertical-centerbias'>
+        <Routing />
+      </div>
+    </RecoilRoot>
   </>
 }
 
--- client/src/Homepage.tsx
+++ client/src/Homepage.tsx
@@ -1,8 +1,64 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './index.css'
+import {
+    RecoilRoot,
+    atom,
+    selector,
+    useRecoilState,
+    useRecoilValue,
+} from 'recoil';
 
 const sampleNames = 'Henry Izak Jarron Ben'.split(' ')
+
+const textState = atom({
+    key: 'textState', // unique ID (with respect to other atoms/selectors)
+    default: '', // default value (aka initial value)
+});
+
+const charCountState = selector({
+    key: 'charCountState', // unique ID (with respect to other atoms/selectors)
+    get: ({ get }) => {
+        const text = get(textState);
+
+        return text.length;
+    },
+});
+
+function CharacterCount() {
+    const count = useRecoilValue(charCountState);
+
+    return <>Character Count: {count}</>;
+}
+
+
+
+function CharacterCounter() {
+    return (
+        <div>
+            <TextInput />
+            <CharacterCount />
+        </div>
+    );
+}
+
+function TextInput() {
+
+    const [text, setText] = useRecoilState(textState);
+
+    const onChange = (event: any) => {
+        setText(event.target.value);
+    };
+
+    return (
+        <div>
+            <input type="text" value={text} onChange={onChange} />
+            <br />
+            Echo: {text}
+        </div>
+    );
+}
+
 
 function renderRoom(data: { name: string }) {
     return (<div>
@@ -18,37 +74,45 @@ function renderRoom(data: { name: string }) {
 }
 
 function RoomList() {
-    return (<div className="roomList">
-
-        <aside>
-            <h1>Existing Rooms</h1>
-        </aside>
-
-        <main>
-            {
-                sampleNames.map((name) => {
-                    return renderRoom({ name: name })
-                })
-            }
-        </main>
+    return (
 
 
-    </div>)
+        <div className="roomList">
+
+            <aside>
+                <h1>Existing Rooms</h1>
+            </aside>
+
+            <main>
+                {
+                    sampleNames.map((name) => {
+                        return renderRoom({ name: name })
+                    })
+                }
+            </main>
+
+
+        </div>)
 }
 
 
 export default function Homepage() {
 
+    const [text, setText] = useRecoilState(textState);
+
     return (<div className="grid-2-horizontal">
 
         <RoomList />
+
+        <CharacterCounter/>
 
         <button
             className="createRoom"
             onClick={
                 () => {
-                    console.log('wowie!!! someone PLEASE impelemnt createThatRoom()!!!!!! T_T')
-                    return "potato";
+                    console.log('TODO Implement createThatRoom()')
+
+                    setText(text + " create room clicked")
                 }
             }>
             Create a room
