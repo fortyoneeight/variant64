92dd07991ecdef5b41801b75aaa38c8e2d80c490
Ben <shumakerbenjamin@gmail.com>
2022-10-16 17:14:07 -0400

    Minor Gameboard Update

--- client/src/components/gameboard.tsx
+++ client/src/components/gameboard.tsx
@@ -2,21 +2,21 @@ import React from "react";
 
 export default function Gameboard(snapshot: any) {
 
-
-    console.log("potato rotato :) we have le mcgriddle")
-
     console.log(snapshot);
 
-    const cells = snapshot.board.size.length * snapshot.board.size.width;
+    const numCells = snapshot.board.size.length * snapshot.board.size.width;
 
     return <div className='gameboard-grid' style={{
             gridTemplateRows: `repeat(${snapshot.board.size.length}, 10vh)`,
             gridTemplateColumns: `repeat(${snapshot.board.size.width}, 10vh)`
         }}>
-            {[...Array(cells)].map((cell: any, i: any) => {  
-                return <div key={i} className='grid-cell' style={{backgroundColor: i % 2 == 1 ? 'white' : '#21823b'}}>
-                    <p style={{fontSize:'4rem'}}></p>
-                </div>
+            {[...Array(snapshot.board.size.length)].map((row: any, i: any) => {
+                return [...Array(snapshot.board.size.width)].map((cell: any, j: any) =>{
+                    const color = i % 2 + j % 2 == 1 ? 'white' : 'blue'  
+                    return <div key={i} className='grid-cell' style={{backgroundColor: color}}>
+                        
+                    </div>
+                }) 
             })}
     </div>
 }
