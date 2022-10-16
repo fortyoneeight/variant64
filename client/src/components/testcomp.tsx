df5022365f742894d895a6d92188223fd4bb1ff7
jarronb <baileyjarron@gmail.com>
2022-10-16 13:10:29 -0500

    [frontend] folders

---
+++ client/src/components/testcomp.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+import '../services';
+function App() {
+  return <div className='testcomp'></div>;
+}
+
+export default App;
---
+++ client/src/page/test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Test() {
+  return <div className='testcomp'></div>;
+}
+
+export default Test;
