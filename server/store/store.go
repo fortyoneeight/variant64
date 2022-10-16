da874c36d3bd129de15226255f2f224fddf1dcab
Izak Fritz <izakfr@umich.edu>
2022-10-16 19:00:22 -0400

    Add player and room store (#14)

---
+++ server/store/store.go
@@ -0,0 +1,49 @@
+package store
+
+import (
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+type Indexable interface {
+	GetID() uuid.UUID
+}
+
+type IndexedStore[T Indexable] struct {
+	DataMap map[uuid.UUID]T
+
+	Mux *sync.RWMutex
+}
+
+func (i *IndexedStore[T]) Lock() {
+	i.Mux.Lock()
+}
+
+func (i *IndexedStore[T]) Unlock() {
+	i.Mux.Unlock()
+}
+
+func (i *IndexedStore[T]) GetAll() []T {
+	list := []T{}
+	for _, val := range i.DataMap {
+		list = append(list, val)
+	}
+	return list
+}
+
+func (i *IndexedStore[T]) GetByID(id uuid.UUID) []T {
+	if val, ok := i.DataMap[id]; ok {
+		return []T{val}
+	}
+
+	return []T{}
+}
+
+func (i *IndexedStore[T]) Store(t T) {
+	i.DataMap[t.GetID()] = t
+}
+
+func (i *IndexedStore[T]) Delete(id uuid.UUID) {
+	delete(i.DataMap, id)
+}
