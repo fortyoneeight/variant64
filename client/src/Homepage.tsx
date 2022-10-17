eb9e1f23a338295c1d637fb7facc85339bbca173
jarronb <baileyjarron@gmail.com>
2022-10-16 19:08:40 -0500

    [frontend] format homepage

--- client/src/Homepage.tsx
+++ client/src/Homepage.tsx
@@ -1,57 +1,54 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import './index.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.css';
 
-const sampleNames = 'Henry Izak Jarron Ben'.split(' ')
+const sampleNames = 'Henry Izak Jarron Ben'.split(' ');
 
-function renderRoom(data: { name: string }, i:any) {
-    return (<div key={i}>
-        <Link
-            to={'/chess'} //`/join/{data.name}`
-            className='grid-2-horizontal-leftbias'>
-
-
-            <span>{data.name}'s room</span> <span className="outline">0/2</span>
-
-        </Link>
-    </div>)
+function renderRoom(data: { name: string }, i: any) {
+  return (
+    <div key={i}>
+      <Link
+        to={'/chess'} //`/join/{data.name}`
+        className='grid-2-horizontal-leftbias'
+      >
+        <span>{data.name}'s room</span> <span className='outline'>0/2</span>
+      </Link>
+    </div>
+  );
 }
 
 function RoomList() {
-    return (<div className="roomList">
+  return (
+    <div className='roomList'>
+      <aside>
+        <h1>Existing Rooms</h1>
+      </aside>
 
-        <aside>
-            <h1>Existing Rooms</h1>
-        </aside>
-
-        <main>
-            {
-                sampleNames.map((name, i) => {
-                    return renderRoom({ name: name }, i)
-                })
-            }
-        </main>
-
-
-    </div>)
+      <main>
+        {sampleNames.map((name, i) => {
+          return renderRoom({ name: name }, i);
+        })}
+      </main>
+    </div>
+  );
 }
 
-
 export default function Homepage() {
+  return (
+    <div className='grid-2-horizontal'>
+      <RoomList />
 
-    return (<div className="grid-2-horizontal">
-
-        <RoomList />
-
-        <button
-            className="createRoom"
-            onClick={
-                () => {
-                    console.log('wowie!!! someone PLEASE impelemnt createThatRoom()!!!!!! T_T')
-                    return "potato";
-                }
-            }>
-            Create a room
-        </button>
-    </div>)
+      <button
+        className='createRoom'
+        onClick={() => {
+          console.log(
+            'wowie!!! someone PLEASE impelemnt createThatRoom()!!!!!! T_T'
+          );
+          return 'potato';
+        }}
+      >
+        Create a room
+      </button>
+    </div>
+  );
 }
